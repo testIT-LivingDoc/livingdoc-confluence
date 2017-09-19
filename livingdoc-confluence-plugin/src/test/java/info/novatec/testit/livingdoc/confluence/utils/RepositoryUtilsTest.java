4b1906a1ac59c4e287860eecc1adbc9792f688ac
José Antonio Delgado <jose1984@gmail.com>
2017-09-19 15:37:08 +0200

    RepositoryUtils Test

---
+++ livingdoc-confluence-plugin/src/test/java/info/novatec/testit/livingdoc/confluence/utils/RepositoryUtilsTest.java
@@ -0,0 +1,20 @@
+package info.novatec.testit.livingdoc.confluence.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RepositoryUtilsTest {
+
+    @Test
+    public void testRepositoryBaseUrl() {
+        final String repositoryBaseUrl = "/display/LIVINGDOC";
+        assertEquals(repositoryBaseUrl, RepositoryUtils.repositoryBaseUrl("", "LIVINGDOC"));
+    }
+
+    @Test
+    public void testBaseTestUrl() {
+        final String baseTestUrl = "/rpc/xmlrpc?handler=livingdoc1#LIVINGDOC";
+        assertEquals(baseTestUrl, RepositoryUtils.baseTestUrl("", "LIVINGDOC"));
+    }
+}
